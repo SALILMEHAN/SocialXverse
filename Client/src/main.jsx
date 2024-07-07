bbd673661d348ed1fb89e16a59bfc4b6c04c132a
SALIL <salilmehan123@gmail.com>
2024-07-07 09:46:07 +0530

    c1

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -10,11 +9,11 @@ import { persistStore } from "redux-persist";
 let persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+  // </React.StrictMode>
 )
